9a48ba2aa10bcf16787363e4f87fb0f92e2999c9
Zach  Smith <zdsmith001@gmail.com>
2019-10-15 18:18:40 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,15 +13,9 @@ class Pokemon
     db.execute(sql, name, type)
   end
 
-  def self.find(id, db)
-    sql = "SELECT * FROM pokemon WHERE id = ?"
-    db.execute(sql, id).map do |match|
-      pokemon_id = match[0]
-      name = match[1]
-      type = match[2]
-      pokemon = Pokemon.new(id: pokemon_id, name: name, type: type, db: db)
-      pokemon
-    end
+  def self.find(poke_id, db)
+    pokedex = db.execute("SELECT * FROM pokemon WHERE id = ?, poke_id")
+    Pokemon.new(id: pokedex[0], name: pokedex[1], type: pokedex[2], db: db)
   end
 
 end
